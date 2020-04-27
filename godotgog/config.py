b2e6db5d714515b52e6e2192cbb2ebd007104e9e
Richi <21098503+pixelriot@users.noreply.github.com>
2020-04-27 14:57:16 +0200

    Update config.py

--- godotgog/config.py
+++ godotgog/config.py
@@ -1,9 +1,9 @@
 def can_build(env, platform):
-    return platform=="windows" or platform=="osx" or platform=="x11"
+    return platform=="windows" or platform=="osx" #or platform=="x11"
 
 
 def configure(env):
-    env.Append( CPPPATH=["#modules/godotgog/sdk/Include/galaxy/"] )
+    env.Append( CPPPATH=["#modules/godotgog/sdk/Include/"] )
     env.Append( LIBPATH=["#modules/godotgog/sdk/Libraries"] )
 
     if env["platform"] == "windows":
@@ -11,8 +11,10 @@ def configure(env):
             env.Append(LINKFLAGS=["Galaxy.lib"])
         else:
             env.Append(LINKFLAGS=["Galaxy64.lib"])
+
     elif env["platform"] == "osx":
         env.Append(LIBS=["libGalaxy.dylib"])
+
     elif env["platform"] == "x11":
         if env["bits"]=="32":
             env.Append(LINKFLAGS=["libGalaxy.so"])
